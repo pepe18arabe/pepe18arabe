9dc28322efd06416e3ff6ee69e3ae06026b9b3c0
pepe18arabe <vinilevada@gmail.com>
2024-08-07 15:36:50 -0300

    Update skatch.js

--- skatch.js
+++ skatch.js
@@ -1,15 +1,7 @@
 function setup() {
-  createCanvas(600, 600);
-  background("white");
+  createCanvas(400, 400);
 }
 
 function draw() {
-  stroke("blue");
-  fill("red");
-
-  // console.log(mouseIsPressed)
-
-  if (mouseIsPressed) {
-    rect(mouseX, mouseY, 20, 35);
-  }
+  background(220);
 }
